0aacda4a762f33b0c3c48374783da4493531d564
hamzalafsioui <hamzalafsioui@gmail.com>
2026-02-01 12:57:57 +0100

    add DbClientService

--- Api/Program.cs
+++ Api/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddOpenApi();
+builder.Services.AddSingleton<DbClientService>();
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
@@ -25,9 +26,15 @@ if (app.Environment.IsDevelopment())
 
 app.UseCors();
 
-app.MapPost("/query", async ([FromBody] QueryRequest request) =>
+app.MapPost("/query", async ([FromBody] QueryRequest request, [FromServices] DbClientService dbService) =>
 {
-  
+    if (string.IsNullOrWhiteSpace(request.Query))
+    {
+        return Results.BadRequest(new { error = "Query cannot be empty" });
+    }
+
+    var result = await dbService.ExecuteQueryAsync(request.DbName ?? "testdb", request.Query);
+    return Results.Ok(result);
 });
 
 app.Run();
